9c8ded611964c39ebe5d6be899a6e95506a16191
nbable0 <nbable0@gmail.com>
2016-08-05 18:41:37 -0400

    add

--- ga_apple_responsive/scripts/index.js
+++ ga_apple_responsive/scripts/index.js
@@ -3,8 +3,3 @@ $(document).ready(function(){
         $('.wrapper #list li').toggle(1000);
     });
 })
-
-$(window).resize(function() {
-    if($(window).width() > 650) // or 768
-        $('.wrapper #list').show();
-});
