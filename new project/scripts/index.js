2cd9a90094cebb7408d634e283fa8f5b336cc331
nbable0 <nbable0@gmail.com>
2016-08-09 16:12:54 -0400

    add

--- new project/scripts/index.js
+++ new project/scripts/index.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-
-})
+$(document).ready(function(){ 
+$( "#list" ).animate({
+  height: 205,
+  width: 350,
+  opacity: 1
+}, 1000, "linear", function() {
+});
+$( "h1" ).animate({
+  height: 150,
+  width: 400,
+  opacity: 1
+}, 1000, "linear", function() {
+});
+});
