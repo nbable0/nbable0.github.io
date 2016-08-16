6f5f394a55bc734db7b55dd3082d30ca06fd9d55
nbable0 <nbable0@gmail.com>
2016-08-16 14:56:32 -0400

    add

--- nb_final_project/scripts/index.js
+++ nb_final_project/scripts/index.js
@@ -1,7 +1,33 @@
-$(document).ready(function(){ 
+/*parsley validation*/
+$(function () {
+  $('#registerForm').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+  })
+  .on('form:submit', function() {
+    return false; 
+  });
+
+$(document).on('scroll', showScroll);
 
 });
 
 
+/*for scroll animation*/
+
+function showScroll(event){
+    if ($(window).scrollTop() >= 150){
+        $('#fade-in').fadeIn(2000);
+    }else{
+        $('#fade-in').fadeOut(2000);
+    }
+    console.log(window.pageYOffset);
+}
 
 
+
+/*for apple page*/
+$(document).ready(function(){
+    $('.box-shadow-menu').on('mouseover',function(){
+        $('.wrapper #list_a li').toggle(1000);
+    });
+})
