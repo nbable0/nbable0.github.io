473ec1d79d93200de441ed80c640b45c7462db97
nbable0 <nbable0@gmail.com>
2016-08-17 14:15:21 -0400

    add

--- nb_final_project/scripts/index.js
+++ nb_final_project/scripts/index.js
@@ -9,20 +9,28 @@ $(function () {
 
 $(document).on('scroll', showScroll);
 
-});
+$(document).on('scroll', showContact);
 
+});
 
 /*for scroll animation*/
 
 function showScroll(event){
-    if ($(window).scrollTop() >= 150){
+    if ($(window).scrollTop() >= 500){
         $('#fade-in').fadeIn(2000);
     }else{
-        $('#fade-in').fadeOut(2000);
+        $('#fade-in').fadeOut(1000);
     }
     console.log(window.pageYOffset);
 }
 
+function showContact(event){
+    if ($(window).scrollTop() >= 1300){
+        $('#fade-contact').fadeIn(2500);
+    }else{
+        $('#fade-contact').fadeOut(1000);
+    }
+}
 
 
 /*for apple page*/
