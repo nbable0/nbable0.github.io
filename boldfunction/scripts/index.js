a2ecec6c9484e0b47e8bf6b09625cccfffa0cec6
nbable0 <nbable0@gmail.com>
2016-08-20 08:37:02 -0400

    add

---
+++ boldfunction/scripts/index.js
@@ -0,0 +1,41 @@
+/*parsley validation*/
+$(function () {
+  $('#registerForm').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+  })
+  .on('form:submit', function() {
+    return false; 
+  });
+
+$(document).on('scroll', showScroll);
+
+$(document).on('scroll', showContact);
+
+});
+
+/*for scroll animation*/
+
+function showScroll(event){
+    if ($(window).scrollTop() >= 450){
+        $('#fade-in').fadeIn(2500);
+    }else{
+        $('#fade-in').fadeOut(1000);
+    }
+    console.log(window.pageYOffset);
+}
+
+function showContact(event){
+    if ($(window).scrollTop() >= 1300){
+        $('#fade-contact').fadeIn(2500);
+    }else{
+        $('#fade-contact').fadeOut(1000);
+    }
+}
+
+
+/*for apple page*/
+$(document).ready(function(){
+    $('.box-shadow-menu').on('mouseover',function(){
+        $('.wrapper #list_a li').toggle(1000);
+    });
+})
